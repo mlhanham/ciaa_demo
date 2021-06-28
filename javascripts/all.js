ab1f630212070b81f3cdab4bdfc4a857ee0d0b02
mlhanham <matthew.hanham@gmail.com>
2021-06-28 17:18:28 +0100

    first commit

---
+++ javascripts/all.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function ($) {
+
+  var $toggle = $('#nav-toggle');
+  var $menu = $('#nav-menu');
+
+  $toggle.click(function() {
+    $(this).toggleClass('is-active');
+    $menu.toggleClass('is-active');
+  });
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    nav:false,
+    dots:false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    smartSpeed: 1500,
+    animateOut: 'fadeOut',
+    responsive:{
+        0:{
+          items:1
+        },
+        600:{
+          items:1
+        },
+        1000:{
+          items:1
+        }
+    }
+  });
+ 
+});
